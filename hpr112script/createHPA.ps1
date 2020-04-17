d71b95feb4c004b5bdf00d05647dae41a211b93b
bachelor112hpr <62650506+bachelor112hpr@users.noreply.github.com>
2020-04-17 15:21:51 +0200

    Add files via upload

---
+++ hpr112script/createHPA.ps1
@@ -0,0 +1,8 @@
+﻿$deployments = ("apigwmm", "apigwms", "apigwwm", "apigwws", "basket-api", "basket-data", "catalog-api", "identity-api", "keystore-data", "locations-api", "marketing-api", "mobileshoppingagg", "nosql-data", "ordering-api", "payment-api", "rabbitmq", "sql-data", "webhooks-api", "webhooks-web", "webmvc", "webshoppingagg", "webspa", "webstatus")
+$targetCPU = Read-Host -prompt "Select target CPU percent"
+$minPods = Read-Host -prompt "Select minimum number of pods"
+$maxPods = Read-Host -prompt "Select maximum number of pods"
+
+foreach ($deployment in $deployments) {
+    kubectl autoscale deployment eshop-$deployment --cpu-percent=$targetCPU --min=$minPods --max=$maxPods
+}
